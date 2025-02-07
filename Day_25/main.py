a8e7914157f90c06c5df5f9c7acf4b00da60f817
codennomad <gabrielferreiralockvieira@gmail.com>
2025-02-07 15:44:07 -0300

    two days added, day 25 in process - Day 24 working w/ files, directories and paths - Day 25 we will work w/ pandas and csv

---
+++ Day_25/main.py
@@ -0,0 +1,17 @@
+import pandas
+# import csv
+
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+
+#com pandas
+data = pandas.read_csv("weather_data.csv")
+print(type(data))
+# print(data)
+# print()
+# print(data["temp"])
