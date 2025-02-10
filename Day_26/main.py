a3afee92a3359882fb14e06c91463a6924306308
codennomad <gabrielferreiralockvieira@gmail.com>
2025-02-10 14:31:51 -0300

    Day 26 complete

--- Day_26/main.py
+++ Day_26/main.py
@@ -1,3 +1,12 @@
 
-number = [2*n for n in range(1,5)]
-print(number)
+# number = [2*n for n in range(1,5)]
+# print(number)
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+word = input("Enter a word: ").upper()
+ouput_list = [phonetic_dict[letter] for letter in word]
+print(ouput_list)
